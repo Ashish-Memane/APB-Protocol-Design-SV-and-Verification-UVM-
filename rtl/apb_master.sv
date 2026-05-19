e38e7b406e6eb4c76d2c45b40653a17e2ae70e32
Ashish Memane <85474782+Ashish-Memane@users.noreply.github.com>
2026-05-19 14:05:46 +0530

    Refactor apb_master module with updated comments
    Updated module header and comments for clarity.

--- rtl/apb_master.sv
+++ rtl/apb_master.sv
@@ -1,3 +1,22 @@
+//////////////////////////////////////////////////////////////////////////////////
+// Company: NA
+// Engineer: Ashish Memane
+// 
+// Create Date: 19.05.2026 12:26:29
+// Design Name: 
+// Module Name: apb_master
+// Project Name: ABP_PROTOCOL_DESIGN
+// Target Devices: 
+// Tool Versions: VIVADO
+// Description: APB_MASTER
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 module apb_master (
     input logic PCLK,
     input logic PRESETn,
