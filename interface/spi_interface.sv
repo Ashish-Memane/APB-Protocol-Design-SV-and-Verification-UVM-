179cf452bd56221dc9fde86cb718a70c907ec760
Ashish Memane <85474782+Ashish-Memane@users.noreply.github.com>
2026-05-30 13:16:35 +0530

    Create spi_interface.sv

---
+++ interface/spi_interface.sv
@@ -0,0 +1,72 @@
+// spi interface
+interface spi_interface(input logic PCLK, input logic PRESETn);
+
+    //--------------------------------------------------
+    // SPI SIGNALS
+    //--------------------------------------------------
+
+    logic miso;
+
+    logic mosi;
+    logic sclk;
+    logic cs;
+
+    //--------------------------------------------------
+    // SPI DRIVER CLOCKING BLOCK
+    //--------------------------------------------------
+
+    clocking spi_drv_cb @(posedge sclk);
+
+        default input #1 output #1;
+
+        //----------------------------------------------
+        // SPI SLAVE RESPONSE
+        //----------------------------------------------
+
+        output miso;
+
+        //----------------------------------------------
+        // DUT GENERATED SIGNALS
+        //----------------------------------------------
+
+        input mosi;
+        input sclk;
+        input cs;
+
+        //----------------------------------------------
+        // APB SIDE
+        //----------------------------------------------
+    endclocking : spi_drv_cb
+
+    //--------------------------------------------------
+    // SPI MONITOR CLOCKING BLOCK
+    //--------------------------------------------------
+
+    clocking spi_mon_cb @(posedge sclk);
+
+        default input #1 output #1;
+
+        //----------------------------------------------
+        // SPI SIGNALS
+        //----------------------------------------------
+
+        input miso;
+        input mosi;
+        input sclk;
+        input cs;
+
+        //----------------------------------------------
+        // APB SIGNALS
+        //----------------------------------------------
+
+    endclocking : spi_mon_cb
+
+    //--------------------------------------------------
+    // MODPORTS
+    //--------------------------------------------------
+
+    modport SPI_DRV_MP (clocking spi_drv_cb);
+
+    modport SPI_MON_MP (clocking spi_mon_cb);
+
+endinterface : spi_interface
